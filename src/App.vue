094fbffa69e9460801634546aec5369b249ff443
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-02-11 17:27:50 -0600

    completed Transaction List component

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,11 @@
   import Balance from './components/Balance.vue';
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import AddTransaction from './components/AddTransaction.vue';
+  import TransactionList from './components/TransactionList.vue';
+
   import {ref, computed} from 'vue';
 
-  const transactions = ref([
-    {id: 1, text: 'Paycheck', amount: 700.00},
-    {id: 2, text: 'Water Bill', amount: -72.83},
-    {id: 3, text: 'Electric Bill', amount: -153.89},
-    {id: 1, text: 'Returned Item', amount: 20.00},
-  ])
+  const transactions = ref([])
 
   const sum = computed(()=>{
     return transactions.value.reduce((acc, x)=>{
@@ -34,6 +31,22 @@
     },0)
   })
 
+  const handleTransaction = (transactionData) => {
+    transactions.value.push({
+      id: generateID(),
+      text: transactionData.text,
+      amount: transactionData.amount,
+    })
+  }
+
+  const generateID = () => {
+    return Math.floor(Math.random()*10000000)
+  }
+
+  const handleDelete = (id) => {
+    transactions.value = transactions.value.filter((x) => x.id !== id)
+  }
+
 </script>
 
 
@@ -42,7 +55,8 @@
   <div class="container">
     <Balance :total="sum"></Balance>
     <IncomeExpenses :income="moneyIn" :expense="-moneyOut"></IncomeExpenses>
-    <AddTransaction></AddTransaction>
+    <AddTransaction @transactionSubmitted="handleTransaction"></AddTransaction>
+    <TransactionList :transactions="transactions" @transactionDeleted="handleDelete"></TransactionList>
   </div>
 
 
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,29 @@
+<script setup>
+    import { defineProps, defineEmits } from 'vue';
+
+    const props = defineProps({
+        transactions: {
+            type: Array,
+            require: true,
+        }
+    })
+
+    const emit = defineEmits(['transactionDeleted'])
+
+    const deleteTransaction = (id) => {
+        emit('transactionDeleted', id)
+    }
+
+</script>
+
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li v-for="x in transactions" v-bind:key="x.id"
+        v-bind:class="z=x.amount < 0 ? 'minus' : 'plus'">
+        {{ x.text }} <span> ${{ x.amount }}</span>
+        <button @click="deleteTransaction(x.id)" class="delete-btn">X</button>
+        </li>
+
+    </ul>
+</template>
