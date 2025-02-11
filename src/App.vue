19a175487064a8d064f3f04caf29a3b9eb3e3bce
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-02-11 17:33:58 -0600

    completed expense tracker

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   import AddTransaction from './components/AddTransaction.vue';
   import TransactionList from './components/TransactionList.vue';
 
-  import {ref, computed} from 'vue';
+  import {ref, computed, onMounted} from 'vue';
 
   const transactions = ref([])
 
@@ -37,6 +37,7 @@
       text: transactionData.text,
       amount: transactionData.amount,
     })
+    saveToLocalStorage()
   }
 
   const generateID = () => {
@@ -45,7 +46,20 @@
 
   const handleDelete = (id) => {
     transactions.value = transactions.value.filter((x) => x.id !== id)
+    saveToLocalStorage()
   }
+
+  const saveToLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactions.value)) 
+  }
+
+  onMounted(() => {
+    const savedTransactions = JSON.parse(localStorage.getItem('transactions')) 
+
+    if(savedTransactions){
+      transactions.value = savedTransactions
+    }
+  })
 
 </script>
 
