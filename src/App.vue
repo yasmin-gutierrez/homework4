7d84384e8bdc2aa0acf7af7c43f7fce50142f0a0
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-02-11 14:44:58 -0600

    Finished Balanced

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import {ref, computed} from 'vue';
+
+  const transactions = ref([
+    {id: 1, text: 'Paycheck', amount: 700.00},
+    {id: 2, text: 'Water Bill', amount: -72.83},
+    {id: 3, text: 'Electric Bill', amount: -153.89},
+    {id: 1, text: 'Returned Item', amount: 20.00},
+  ])
+
+  const sum = computed(()=>{
+    return transactions.value.reduce((acc, x)=>{
+      return acc+x.amount
+    },0)
+  })
+</script>
+
+
+<template>
+  <Header></Header>
+  <div class="container">
+    <Balance :total="sum"></Balance>
+  </div>
+
+
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <h2>Expense Tracker 2024</h2>
+
+</template>
